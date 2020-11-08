8fb578c2fd9354c021ff1d3be7853e9b06b7b263
aba450 <54678566+aba450@users.noreply.github.com>
2020-11-08 15:45:01 -0500

    Updates

---
+++ precache-manifest.cec2a0b2eda272a55d1f35f85413ec30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe771fa09e6e68f3b7d3d116302fd3eb",
+    "url": "/movie-frontend/index.html"
+  },
+  {
+    "revision": "cc067eea2bc487dd7e39",
+    "url": "/movie-frontend/static/css/main.167fc0d5.chunk.css"
+  },
+  {
+    "revision": "849daf4b73b4a0fddf4e",
+    "url": "/movie-frontend/static/js/2.e85338fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-frontend/static/js/2.e85338fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc067eea2bc487dd7e39",
+    "url": "/movie-frontend/static/js/main.bb760e06.chunk.js"
+  },
+  {
+    "revision": "e095e3a9420ab556031f",
+    "url": "/movie-frontend/static/js/runtime-main.7b2f5f8a.js"
+  }
+]);
